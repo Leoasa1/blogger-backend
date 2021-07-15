19548e2719638ff0f694350cc530d14a9c457b59
Leo Asadourian <leoasadourian@Leos-MacBook-Pro.local>
2021-07-15 12:28:31 -0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a17ac2e09f6df043267d8e159c07e537'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6e036740-de67-4874-aed3-4908e2693866'
+};
